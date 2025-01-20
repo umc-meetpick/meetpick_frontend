d2049516fd90f053c00ce0f32186993794982cf3
taeyoung0524 <miseoseo1@naver.com>
2025-01-20 17:09:15 +0900

    CHORE : BasicNavbar 코드 수정

--- src/components/navbar/BasicNavbar.tsx
+++ src/components/navbar/BasicNavbar.tsx
@@ -2,41 +2,52 @@ import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from "react-router-dom";
 import { IoChevronBackOutline } from "react-icons/io5";
+import { GoBell } from "react-icons/go";
 
 interface TitleProps{
     title:string;
+    before?: boolean;
+    bell?:boolean;
 }
 
-const BasicNavbar: React.FC<TitleProps> = ({title}) =>{
+const BasicNavbar: React.FC<TitleProps> = ({title, before, bell}) =>{
     const navigate = useNavigate();
     const handleGoBack = () => {
         navigate(-1);
       };
     return(
         <Container>
-            <IconPosition onClick={handleGoBack}><IoChevronBackOutline size={24}/></IconPosition>
+            {before && <IconPosition onClick={handleGoBack}><IoChevronBackOutline size={24}/></IconPosition>}
             <Title>{title}</Title>
+            {bell && <IconPosition2 onClick={()=>navigate("/alarm")}><GoBell size={24}/></IconPosition2>}
         </Container>
     )
 }
 export default BasicNavbar
 
 const Container = styled.div`
-    width:393px;
+    width:100%;
+    max-width:393px;
     height:60px;
-    align-items: center;
     background-color:white;
+    display:flex;
+    justify-content: center;
+    align-item:center;
 `;
 const IconPosition = styled.div`
     position:relative;
     top:20px;
-    left:33px;
+    left:-20%;
+`;
+const IconPosition2 = styled.div`
+    position:relative;
+    top:20px;
+    left:20%;
 `;
 const Title = styled.div`
     font-size:17px;
-    font-weight:bold;
+    font-weight:500;
     color:black;
-    display:flex;
-    justify-content: center;
-    margin-top:-10px;
+    position:relative;
+    top:20px;
 `
