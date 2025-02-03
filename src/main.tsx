da6a09fc28d2c2c8e9b4ebac69f4393ec3c8f189
seoyoon lee <leeseoyoon01@gmail.com>
2025-02-03 14:53:12 +0900

    REFACTOR: 홈 api 연결 구조 수정

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,9 @@ import { ChatProvider } from "./context/useChatContext.tsx"
 import { FoodProfileContextProvider } from './context/foodProfileInfo.tsx'
 import { ExerciseProfileContextProvider } from './context/exerciseInfoContext.tsx'
 import { StudyProfileContextProvider } from './context/studyInfoContext.tsx'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,7 +18,9 @@ createRoot(document.getElementById('root')!).render(
         <FoodProfileContextProvider>
           <ExerciseProfileContextProvider>
             <StudyProfileContextProvider>
-              <App />
+              <QueryClientProvider client={queryClient}>
+                <App />
+              </QueryClientProvider>
             </StudyProfileContextProvider>
           </ExerciseProfileContextProvider>
         </FoodProfileContextProvider>
