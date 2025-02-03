27ba157ca07ebbc14161b658bb7fd4787851dc60
taeyoung0524 <miseoseo1@naver.com>
2025-02-03 13:23:32 +0900

    FIX : vite.config.js 코드 수정

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.svg'], // 이미지 처리
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://3.38.151.77:8080',
+        changeOrigin: true,
+        asecure: false,
+        credentials: true,
+        rewrite: (path) => path,
+      },
+    },
+  },
 })
