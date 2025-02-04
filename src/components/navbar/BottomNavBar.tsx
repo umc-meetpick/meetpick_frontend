835e3e48652af99b1a4b1b7b955dbeebcdf04538
taeyoung0524 <miseoseo1@naver.com>
2025-02-05 00:03:04 +0900

    FEAT : useLocation 사용 -> 해당 페이지 active = true이면 icon 색상 변경

--- src/components/navbar/BottomNavBar.tsx
+++ src/components/navbar/BottomNavBar.tsx
@@ -1,29 +1,33 @@
-import React from 'react';
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { GrHomeRounded } from "react-icons/gr";
 import { GoPeople } from "react-icons/go";
 import { GoPerson } from "react-icons/go";
+import { useLocation } from "react-router-dom";
 
 const BottomNavBar = () => {
+  const location = useLocation(); // 현재 URL 경로 가져오기
+
   return (
     <BottomNav>
-      <NavItem>
+     <NavItem active={location.pathname === "/"}>
         <StyledLink to="/">
-            <GrHomeRounded size={24} color ="gray"/>
-            <Title>홈</Title>
+          <GrHomeRounded size={24} color={location.pathname === "/" ? "#1A6AFF" : "gray"} />
+          <Title active={location.pathname === "/"}>홈</Title>
         </StyledLink>
       </NavItem>
-      <NavItem>
+
+      <NavItem active={location.pathname === "/looking"}>
         <StyledLink to="/looking">
-            <GoPeople size={24} color="gray"/>
-            <Title>매칭</Title>
+          <GoPeople size={24} color={location.pathname === "/looking" ? "#1A6AFF" : "gray"} />
+          <Title active={location.pathname === "/looking"}>매칭</Title>
         </StyledLink>
       </NavItem>
-      <NavItem>
+
+      <NavItem active={location.pathname === "/my"}>
         <StyledLink to="/my">
-          <GoPerson size={24} color="gray"/>
-          <Title>My</Title>
+          <GoPerson size={24} color={location.pathname === "/my" ? "#1A6AFF" : "gray"} />
+          <Title active={location.pathname === "/my"}>My</Title>
         </StyledLink>
       </NavItem>
     </BottomNav>
@@ -48,18 +52,19 @@ const BottomNav = styled.div`
   background-color:white;
 `
 
-const NavItem = styled.div`
+const NavItem = styled.div<{active?:boolean}>`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  color: #000;
+  color: ${({ active }) => (active ? "#007AFF" : "gray")}; /* 활성화된 경우 색상 변경 */
 `
 
-const Title = styled.p`
+const Title = styled.p<{active?:boolean}>`
   margin: 5px 0 0;
   font-size: 12px;
   text-align:center;
+  color: ${({ active }) => (active ? "#007AFF" : "gray")}; /* 활성화된 경우 색상 변경 */
 `
 const StyledLink = styled(Link)`
   display: flex;
