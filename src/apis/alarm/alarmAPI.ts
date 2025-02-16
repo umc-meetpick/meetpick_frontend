2d4743faafa2f738358c7aaea2154ff05b504892
taeyoung0524 <miseoseo1@naver.com>
2025-02-16 14:13:09 +0900

    FIX : 알람 페이지 mateType 한글로 수정

--- src/apis/alarm/alarmAPI.ts
+++ src/apis/alarm/alarmAPI.ts
@@ -3,7 +3,7 @@ import { useQuery } from "@tanstack/react-query";
 
 interface AlarmData {
     mappingId :number;
-    mateType:"MEAL" | "EXERCISE" | "STUDY";
+    mateType:"혼밥" | "운동" | "공부";
     content:string;
     createdAt : string;
 }
@@ -13,9 +13,13 @@ export const useAlarmList = (mateType :string)=> {
         queryKey:["alarmList", mateType],
         queryFn: async() => {
             const {data} = await axiosInstance.get(`/api/matches/alarm?mateType=${mateType}`, {
-                params:{mateType},
+                params: {
+                mateType,
+                page:0,
+                size:10,
+                }
             });
-            return data.result; // API 응답에서 result 부분만 반환 
+            return data?.result||[]; // API 응답에서 result 부분만 반환 
         },
         staleTime:60000, // 1분동안 데이터 유지 
     });
