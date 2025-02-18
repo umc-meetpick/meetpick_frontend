c46ad2c218cbeea5d4fe2ba154f0601d9183396f
seoyoon lee <leeseoyoon01@gmail.com>
2025-02-18 11:52:14 +0900

    FIX: 불필요한 react임포트 등 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import  { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { ProfileContextProvider } from './context/profileInfoContext.tsx'
-import { ChatProvider } from "./context/useChatContext.tsx"
-import { FoodProfileContextProvider } from './context/foodProfileInfo.tsx'
-import { ExerciseProfileContextProvider } from './context/exerciseInfoContext.tsx'
-import { StudyProfileContextProvider } from './context/studyInfoContext.tsx'
+import App from './App'
+import { ProfileContextProvider } from './context/profileInfoContext'
+import { ChatProvider } from "./context/useChatContext"
+import { FoodProfileContextProvider } from './context/foodProfileInfo'
+import { ExerciseProfileContextProvider } from './context/exerciseInfoContext'
+import { StudyProfileContextProvider } from './context/studyInfoContext'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
